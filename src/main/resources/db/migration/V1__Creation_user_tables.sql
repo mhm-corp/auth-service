7fffad7ba873331e5a9e24794fde956e8e906b0e
Helen Acero <hepacest@gmail.com>
2025-03-20 10:30:49 -0500

    - To start development, the security library is commented out. This topic will be revisited later in the development process. - The script for creating the user table is added.

---
+++ src/main/resources/db/migration/V1__Creation_user_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id VARCHAR(30) PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    birth_date DATE,
+    phone_number VARCHAR(20) NOT NULL
+);
