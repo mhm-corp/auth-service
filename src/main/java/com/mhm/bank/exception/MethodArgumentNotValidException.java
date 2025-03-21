3a61b7cdce2bb0893454257c2e8e4ebce38e66de
Helen Acero <hepacest@gmail.com>
2025-03-21 12:18:27 -0500

    An exception controller is added to display clearer messages to the user.

---
+++ src/main/java/com/mhm/bank/exception/MethodArgumentNotValidException.java
@@ -0,0 +1,7 @@
+package com.mhm.bank.exception;
+
+public class MethodArgumentNotValidException  extends Exception {
+    public MethodArgumentNotValidException(String message) {
+        super(message);
+    }
+}
